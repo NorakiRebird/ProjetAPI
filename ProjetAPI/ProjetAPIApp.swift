c61876e7ad7455dc5195c797c94a7837b1e98e0f
NorakiRebird <Rayann_Chaghla@outlook.fr>
2025-07-17 19:02:56 +0200

    Initial Commit

---
+++ ProjetAPI/ProjetAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetAPIApp.swift
+//  ProjetAPI
+//
+//  Created by Rayann chaghla on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjetAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
